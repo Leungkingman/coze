6700bf22e8bd2557312b42eb55daf0207821f10d
leungkingman <leungkingman1991@gmail.com>
2025-02-28 00:37:31 +0800

    clear files

--- api/app/router.js
+++
@@ -1,7 +0,0 @@
-/**
- * @param {Egg.Application} app - egg application
- */
-module.exports = app => {
-  const { router, controller } = app;
-  router.post('/wechat/getOpenId', controller.wechat.getOpenId);
-};
